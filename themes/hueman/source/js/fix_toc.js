4bc0ac5084e5f78a412b9070c96c86a7a19c93f4
liming <cqupt_hm@163.com>
2019-04-18 20:41:24 +0800

    文章目录固定在顶部功能完成

--- themes/hueman/source/js/fix_toc.js
+++ themes/hueman/source/js/fix_toc.js
@@ -1,11 +1,19 @@
-// 固定目录js文件，目前未生效
+// 固定目录js文件
 var lb_article = (function ($) {
-    var _sideBarShow=function () {
-        var scrollTop=$("body").scrollTop();
-        if(scrollTop>160){
-            $("#toc").addClass("toc-articleP");
-        }else if(scrollTop<=160&&$("#toc").has("toc-articleP")){
-            $("#toc").removeClass("toc-articleP");
+    var _sideBarShow = function () {
+        //toc-parent div: 这里面只包装了toc div，为了计算原本toc目录div在window里面的位置
+        //toc-parent距离窗口顶部的距离
+        var toc_top = $('#toc-parent').offset().top;
+        //文档距离窗口顶部的距离
+        var doc_scrollTop = $(document).scrollTop();
+        //相减得到toc-parent当前距离窗口顶端的距离
+        var toc_scrollTop = toc_top - doc_scrollTop;
+
+        //如果目录处于顶部，则添加toc-article样式将toc固定在顶部
+        if (toc_scrollTop <= 0) {
+            $("#toc").addClass("toc-article");
+        } else if (toc_scrollTop > 0 && $("#toc").has("toc-article")) {
+            $("#toc").removeClass("toc-article");
         }
     };
     var pub = {
